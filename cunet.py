cb1201e9b77dfb152d9fb65c13a3fa77f15fe4df
Yangang Cao <46161052+YangangCao@users.noreply.github.com>
2021-12-02 10:27:48 +0800

    Update cunet.py

--- cunet.py
+++ cunet.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     K = 8 # zeros padding frame number
     x = torch.rand(1, 8, 512, 249) #the 4 seconds original input
     print("input shape:", x.shape)
-    prefix_frames = torch.rand(1, 8, 512, K) # K zeros prefix frames 
+    prefix_frames = torch.zeros(1, 8, 512, K) # K zeros prefix frames 
     x = torch.cat((prefix_frames, x), 3)
     print("output shape:",layer(x)[:,:,:-K,:].shape) # stripe the last K frames
     total_num = sum(p.numel() for p in layer.parameters())

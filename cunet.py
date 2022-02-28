f413ddf51f893ba8dff850eb37c9baccbafaef34
Yangang Cao <46161052+YangangCao@users.noreply.github.com>
2022-02-28 10:33:39 +0800

    Update cunet.py
    update from author of paper

--- cunet.py
+++ cunet.py
@@ -1,4 +1,3 @@
-""" Yangang Cao 2021.12.1 15:30"""
 import torch
 import torch.nn as nn
 
@@ -22,7 +21,6 @@ class CausalConvBlock(nn.Module):
         2D Causal convolution.
         Args:
             x: [B, C, F, T]
-
         Returns:
             [B, C, F, T]
         """
@@ -48,7 +46,6 @@ class CausalTransConvBlock(nn.Module):
         2D Causal convolution.
         Args:
             x: [B, C, F, T]
-
         Returns:
             [B, C, F, T]
         """
@@ -63,7 +60,7 @@ class CUNET(nn.Module):
     def __init__(self):
         super(CUNET, self).__init__()
         self.conv_block_1 = CausalConvBlock(8, 32, (6, 2), (2, 1))
-        self.conv_block_2 = CausalConvBlock(32, 32, (6, 2), (2, 1))
+        self.conv_block_2 = CausalConvBlock(32, 32, (7, 2), (2, 1))
         self.conv_block_3 = CausalConvBlock(32, 64, (7, 2), (2, 1))
         self.conv_block_4 = CausalConvBlock(64, 64, (6, 2), (2, 1))
         self.conv_block_5 = CausalConvBlock(64, 96, (6, 2), (2, 1))
@@ -77,9 +74,18 @@ class CUNET(nn.Module):
         self.tran_conv_block_4 = CausalTransConvBlock(96 + 96, 96, (6, 2), (2, 1))
         self.tran_conv_block_5 = CausalTransConvBlock(96 + 64, 64, (6, 2), (2, 1))
         self.tran_conv_block_6 = CausalTransConvBlock(64 + 64, 64, (7, 2), (2, 1))
-        self.tran_conv_block_7 = CausalTransConvBlock(64 + 32, 32,  (6, 2), (2, 1))
-        self.tran_conv_block_8 = CausalTransConvBlock(32 + 32, 8,  (6, 2), (2, 1))
-        self.dense = nn.Linear(512, 512)
+        self.tran_conv_block_7 = CausalTransConvBlock(64 + 32, 32,  (7, 2), (2, 1))
+        self.tran_conv_block_8 = CausalTransConvBlock(32 + 32, 32,  (6, 2), (2, 1))
+        self.last_conv_block = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=8,
+                kernel_size=1,
+                stride=1),
+            nn.BatchNorm2d(num_features=8),
+            nn.Dropout2d(0.5),
+            nn.LeakyReLU(inplace=True)) # author of paper said do this
+        self.dense = nn.Sequential(nn.Linear(514, 514),nn.Sigmoid())
 
     def forward(self, x):
         e1 = self.conv_block_1(x)
@@ -99,18 +105,42 @@ class CUNET(nn.Module):
         d = self.tran_conv_block_6(torch.cat((d, e3), 1))
         d = self.tran_conv_block_7(torch.cat((d, e2), 1))
         d = self.tran_conv_block_8(torch.cat((d, e1), 1))
-        d = d.permute(0,1,3,2)
-        d = self.dense(d)
+        d = self.last_conv_block(d)
+        d = d.permute(0,1,3,2)[:,:,:-8] # author of paper said do this
+        d = self.dense(d).permute(0,1,3,2)
         return d
+
+def l2_norm(s1, s2):
+    norm = torch.sum(s1 * s2, -1, keepdim=True)
+    return norm
+
+
+def si_snr(s1, s2, eps=1e-8):
+    s1_s2_norm = l2_norm(s1, s2)
+    s2_s2_norm = l2_norm(s2, s2)
+    s_target = s1_s2_norm / (s2_s2_norm + eps) * s2
+    e_nosie = s1 - s_target
+    target_norm = l2_norm(s_target, s_target)
+    noise_norm = l2_norm(e_nosie, e_nosie)
+    snr = 10 * torch.log10(target_norm / (noise_norm + eps) + eps)
+    return torch.mean(snr)
+
+
+def snr_loss(inputs, label):
+    return -(si_snr(inputs, label))
+
+def MAE_loss(inputs, label):
+    diff = torch.abs(inputs - label)
+    return diff.sum()/inputs.shape[-1]
 
 
 if __name__ == '__main__':
     layer = CUNET()
     K = 8 # zeros padding frame number
-    x = torch.rand(1, 8, 512, 249) #the 4 seconds original input
+    x = torch.rand(1, 8, 514, 249) #the 4 seconds original input
     print("input shape:", x.shape)
-    prefix_frames = torch.zeros(1, 8, 512, K) # K zeros prefix frames 
-    x = torch.cat((prefix_frames, x), 3)
-    print("output shape:",layer(x)[:,:,:-K,:].shape) # strip the last K frames
+    prefix_frames = torch.zeros(1, 8, 514, K) # K zeros prefix frames 
+    y=torch.cat(( x,prefix_frames),3)
+    print("output shape:",layer(y).shape)
     total_num = sum(p.numel() for p in layer.parameters())
     print(total_num)
